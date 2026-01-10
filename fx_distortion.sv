82f37b9562a60f86d6be13153ea5ad2098fceb27
Karsten Kirby Uy <karsten.uy@gmail.com>
2026-01-09 20:53:59 -0800

    init timing met

--- fx_distortion.sv
+++ fx_distortion.sv
@@ -33,6 +33,7 @@ module fx_distortion #(
     assign drive_gain = 16'h0100 + ({8'h00, fx_drive} << 2); 
 
     logic signed [31:0] distorted_signal[1:0];
+    logic signed [31:0] distorted_signal_reg[1:0];
     logic signed [63:0] mixed_signal[1:0];
     
     localparam signed [31:0] ONE      = 32'sd32768;
@@ -79,6 +80,7 @@ module fx_distortion #(
     logic signed [31:0] x_raw[1:0];
     logic signed [31:0] x[1:0];
     logic signed [63:0] x_sq[1:0];      // 32×32 = 64 bits needed
+    logic signed [63:0] x_sq_reg[1:0];      // 32×32 = 64 bits needed
     logic signed [96:0] x_cb_tmp[1:0];      // 64×32 = up to 96, but we shift
     logic signed [96:0] x_cb_tmp_reg[1:0];      // 64×32 = up to 96, but we shift
     logic signed [63:0] x_cb[1:0];      // 64×32 = up to 96, but we shift
@@ -107,7 +109,11 @@ module fx_distortion #(
     always_comb begin
         for (int i = 0; i < 2; i++) begin  
             // 3. Cubic (all 32-bit inputs, 64-bit intermediates)
-            x_cb_tmp[i] = $signed(x[i]) * $signed(x[i]) * $signed(x[i]); // Back to Q15
+            x_sq[i] = $signed(x[i]) * $signed(x[i]);
+
+            // x_cb_tmp[i] = $signed(x[i]) * $signed(x[i]) * $signed(x[i]); // Back to Q15
+            x_cb_tmp[i] = $signed(x_sq_reg[i]) * $signed(x[i]);
+
             cubic_term[i] = $signed(x_cb_tmp_reg[i] >>> 30);  // Q15
         end
     end
@@ -133,7 +139,8 @@ module fx_distortion #(
             
             // Mix: dry + (wet - dry) * mix
             mix_res[i] = (dry_signal_32[i]) + 
-                        (((distorted_signal[i] - (dry_signal_32[i])) * 
+                        // (((distorted_signal[i] - (dry_signal_32[i])) * 
+                        (((distorted_signal_reg[i] - (dry_signal_32[i])) * 
                         $signed({1'b0, fx_mix})) >>> 8);
             
             mixed_signal[i] = mix_res[i]; // scale back down
@@ -145,10 +152,13 @@ module fx_distortion #(
     always_ff @(posedge clk) begin
         if (!reset_n) begin
             audio_out <= '0;
-            product_shifted_reg[0] <= '0;
-            product_shifted_reg[1] <= '0;
-            x_cb_tmp_reg[0] <= '0;
-            x_cb_tmp_reg[1] <= '0;
+            
+            for (int i = 0; i < 2; i++) begin
+                product_shifted_reg[i] <= '0;
+                x_sq_reg[i] <= '0;
+                x_cb_tmp_reg[i] <= '0;
+                distorted_signal_reg[i] <= '0;
+            end
         end else if (sample_en) begin
             for (int i = 0; i < 2; i++) begin
                 audio_out[i] <= sat16(mixed_signal[i]);
@@ -156,9 +166,10 @@ module fx_distortion #(
                 // Pipeline
                 product_shifted_reg[i] <= product_shifted[i];
                 x_cb_tmp_reg[i] <= x_cb_tmp[i];
+                x_sq_reg[i] <= x_sq[i];
+                distorted_signal_reg[i] <= distorted_signal[i];
             end
             // audio_out = audio_in;
-
         end
     end
 
