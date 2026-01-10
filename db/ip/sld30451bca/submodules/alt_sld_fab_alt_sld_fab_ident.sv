de7699df00a4960eba7f8390f6b290947f577ed2
Karsten Kirby Uy <karsten.uy@gmail.com>
2026-01-09 21:22:48 -0800

    extra EX added

--- db/ip/sld30451bca/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ db/ip/sld30451bca/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 6b8e97f70882de9662ec
+//   DESIGN_HASH   : bf5ebc3a90243058dc06
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h6b8e97f70882de9662ec };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hbf5ebc3a90243058dc06 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
